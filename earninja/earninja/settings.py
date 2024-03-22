580043ca38a04b2ee32fdabb86c4986835499c31
Tomasz Grykias <tgrykias@pm.me>
2024-03-22 18:29:08 +0100

    add favicon

--- earninja/earninja/settings.py
+++ earninja/earninja/settings.py
@@ -135,6 +135,7 @@ MEDIA_URL = 'media/'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
