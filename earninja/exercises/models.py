b7cf942fa9c13097cd80d04768366df0f6ff94de
Tomasz Grykias <tgrykias@pm.me>
2024-03-25 22:14:52 +0100

    add models representing intervals exercise with question and answers

--- earninja/exercises/models.py
+++ earninja/exercises/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 from exercises.audio_file_path_manager import AudioFilePathManager
 
@@ -6,6 +7,9 @@ from exercises.audio_file_path_manager import AudioFilePathManager
 class Interval(models.Model):
     num_semitones = models.IntegerField()
     name = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['num_semitones']
 
     def __str__(self):
         return self.name
@@ -26,3 +30,35 @@ class IntervalInstance(models.Model):
         # https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models.FileField.storage
         # https://docs.djangoproject.com/en/4.2/ref/files/storage/#django.core.files.storage.Storage.url
         return self.audio.field.storage.url(audio_path)
+
+
+class IntervalsExercise(models.Model):
+    question = models.ForeignKey(IntervalInstance, null=True, on_delete=models.SET_NULL, related_name="exercises_with_interval_instance_as_question")
+    answers = models.ManyToManyField(IntervalInstance, through="IntervalAnswer", related_name="exercises_with_interval_instance_as_answer")
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    settings = models.OneToOneField("IntervalsExerciseSettings", null=True, on_delete=models.SET_NULL, related_name="exercise")
+
+    def __str__(self):
+        return f'exercise for user: {self.user}'
+
+
+class IntervalAnswer(models.Model):
+    interval_instance = models.ForeignKey(IntervalInstance, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(IntervalsExercise, on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.interval_instance.interval.name
+
+
+class IntervalsExerciseSettings(models.Model):
+    lowest_octave = models.IntegerField()
+    highest_octave = models.IntegerField()
+    allowed_intervals = models.ManyToManyField(Interval)
+
+    def __str__(self):
+        if hasattr(self, "exercise"):
+            return f'settings for {self.exercise}'
+        else:
+            return f'settings (id={self.id}), no exercise'
--- earninja/exercises/tests/test_models.py
+++ earninja/exercises/tests/test_models.py
@@ -1,12 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from exercises.models import (
     Interval,
     IntervalInstance,
+    IntervalsExercise,
+    IntervalsExerciseSettings,
+    IntervalAnswer,
 )
 
 
-class IntervalTests(TestCase):
+class IntervalModelTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         Interval.objects.create(name="b3", num_semitones=3)
@@ -20,7 +24,7 @@ class IntervalTests(TestCase):
         self.assertEqual(str(interval_5), "5")
 
 
-class IntervalInstanceTests(TestCase):
+class IntervalInstanceModelTests(TestCase):
     def setUp(self):
         self.interval_b3 = Interval.objects.create(name="b3", num_semitones=3)
 
@@ -40,3 +44,94 @@ class IntervalInstanceTests(TestCase):
 
         self.assertEqual(interval_instance_1.get_audio_url(), "/media/audio/interval_1.mp3")
         self.assertEqual(interval_instance_2.get_audio_url(), "/media/audio/interval_2.mp3")
+
+
+class IntervalsExerciseModelTests(TestCase):
+    def setUp(self):
+        settings = IntervalsExerciseSettings.objects.create(
+            lowest_octave=3, 
+            highest_octave=5,
+        )
+        settings.allowed_intervals.set([
+            Interval.objects.create(name="b3", num_semitones=3),
+            Interval.objects.create(name="#4", num_semitones=6),
+            Interval.objects.create(name="5", num_semitones=7)
+        ])
+        question = IntervalInstance.objects.create(start_note=4*12, interval=settings.allowed_intervals.get(name="b3"))
+        answers = [
+            question,
+            IntervalInstance.objects.create(start_note=4*12, interval=settings.allowed_intervals.get(name="#4")),
+            IntervalInstance.objects.create(start_note=4*12, interval=settings.allowed_intervals.get(name="5")),
+        ]
+        User = get_user_model()
+        test_user = User.objects.create_user(username='test_user', password='r6S6FrpHzFqf')
+        test_user.save()
+        self.exercise = IntervalsExercise.objects.create(
+            question=question,
+            user=test_user,
+            settings=settings,
+        )
+        self.exercise.answers.set(answers)
+        answer = IntervalAnswer.objects.get(exercise=self.exercise, interval_instance=question)
+        answer.is_correct = True
+        answer.save()
+    
+    def test_object_name_is_exercise_for_user(self):
+        self.assertEqual(str(self.exercise), 'exercise for user: test_user')
+    
+    def test_through_table_object_name_is_interval_name(self):
+        answer_interval_instance = self.exercise.answers.get(interval__name="#4")
+        answer = IntervalAnswer.objects.get(exercise=self.exercise, interval_instance=answer_interval_instance)
+        self.assertEqual(str(answer), '#4')
+    
+    def test_deleting_answer_does_not_delete_exercise(self):
+        self.assertEqual(self.exercise.answers.count(), 3)
+        self.exercise.answers.get(interval__name="#4").delete()
+
+        self.assertEqual(IntervalsExercise.objects.count(), 1)
+        exercise = IntervalsExercise.objects.get(id=1)
+        self.assertEqual(exercise.answers.count(), 2)
+    
+    def test_deleting_question_does_not_delete_exercise(self):
+        self.exercise.answers.get(interval__name="b3").delete()
+
+        self.assertEqual(IntervalsExercise.objects.count(), 1)
+        exercise = IntervalsExercise.objects.get(id=1)
+        self.assertEqual(exercise.question, None)
+
+    def test_deleting_user_deletes_exercise(self):
+        self.exercise.user.delete()
+        self.assertEqual(IntervalsExercise.objects.count(), 0)
+    
+
+class IntervalsExerciseSettingsModelTests(TestCase):
+    def setUp(self):
+        self.settings = IntervalsExerciseSettings.objects.create(
+            lowest_octave=3, 
+            highest_octave=5,
+        )
+        self.settings.allowed_intervals.set([
+            Interval.objects.create(name="b3", num_semitones=3),
+            Interval.objects.create(name="#4", num_semitones=6),
+            Interval.objects.create(name="5", num_semitones=7)
+        ])
+
+    def test_object_name_is_as_expected_with_no_exericse(self):
+        self.assertEqual(
+            str(self.settings),
+            "settings (id=1), no exercise"
+        )
+    
+    def test_object_name_is_as_expected_with_exericse(self):
+        User = get_user_model()
+        test_user = User.objects.create_user(username='test_user', password='r6S6FrpHzFqf')
+        test_user.save()
+        self.exercise = IntervalsExercise.objects.create(
+            question=None,
+            user=test_user,
+            settings=self.settings,
+        )
+        self.assertEqual(
+            str(self.settings),
+            "settings for exercise for user: test_user"
+        )
